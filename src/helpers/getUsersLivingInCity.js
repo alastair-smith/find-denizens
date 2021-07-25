b25906bf9ea86c936c988262f8e8b65d27318379
Alastair Smith <al.smith.dev@gmail.com>
2021-07-25 13:50:43 +0100

    refactor: made json response consistent
    Put the keys in the same order for all the users

--- src/helpers/getUsersLivingInCity.js
+++ src/helpers/getUsersLivingInCity.js
@@ -4,7 +4,7 @@ const { USERS_API } = require('../constants')
 module.exports = async city => {
   const url = `${USERS_API.BASE_URL}${USERS_API.USERS_LIVING_IN_CITY_PATH.replace('{city}', city)}`
   const users = (await get(url)).data
-  const usersWithFullDetail = users.map(user => ({ city, ...user }))
+  const usersWithFullDetail = users.map(user => ({ ...user, city }))
 
   return usersWithFullDetail
 }
